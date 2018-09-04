fb822069d7f00568c1f23fbf8d684ab832069380
Tim Remeeus <t.remeeus@multisafepay.local>
2018-09-04 13:57:03 +0200

    greater than

--- Tests/BasicsTest.php
+++ Tests/BasicsTest.php
@@ -22,13 +22,62 @@ class BasicsTest extends TestCase
         $this->assertNotEmpty($array);
     }
 
+//  Check if the variables are the same value
     public function testAssertEquals()
     {
         $varOne = 500;
         $varTwo = 500;
 
+//      I Expect var 1 and 2 to be the same value
         $this->assertEquals($varOne, $varTwo);
     }
 
+//  Check if the variables are NOT the same value
+    public function testAssertNotEquals()
+    {
+        $varOne = 500;
+        $varTwo = 600;
 
+//      I expect var 1 and 2 not to be the same value
+        $this->assertNotEquals($varOne, $varTwo);
+    }
+
+//  Check if the outcome is TRUE
+    public function testAssertTrue()
+    {
+        $myOutcomeData = True;
+
+//      I expect that my return is equal to TRUE
+        $this->assertTrue($myOutcomeData);
+    }
+
+//  Check if the outcome is False
+    public function testAssertFalse()
+    {
+        $myOutcomeData = False;
+
+//      I expect that my return is equal to False
+        $this->assertFalse($myOutcomeData);
+    }
+
+//  Check if the data is greater than the other
+    public function testAssertGreaterThan()
+    {
+        $varOne = 10;
+        $varTwo = 25;
+
+//      I expect varTwo to be bigger than varOne
+        $this->assertGreaterThan($varOne, $varTwo);
+
+    }
+//  Check if the data is greater than or Equals to the other
+    public function testAssertGreaterThanOrEqual()
+    {
+        $varOne = 10;
+        $varTwo = array_rand([10,25], 1);
+
+//      I expect varTwo to be bigger than varOne
+        $this->assertGreaterThanOrEqual($varOne, $varTwo);
+
+    }
 }
