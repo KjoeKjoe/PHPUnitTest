10f870567a6d141d7223e03aa6eb31cb768476f0
Tim Remeeus <t.remeeus@multisafepay.local>
2018-09-04 15:11:44 +0200

    added swapi guzzle test

--- Tests/JsonTest.php
+++ Tests/JsonTest.php
@@ -4,11 +4,35 @@ use PHPUnit\Framework\TestCase;
 
 class JsonTest extends TestCase
 {
+    private $http;
+
+    public function setUp()
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'swapi.co/api']);
+    }
+
+    public function tearDown() {
+        $this->http = null;
+    }
+
     public function testJsonStringEquals()
     {
         $this->assertJsonStringEqualsJsonString(
             json_encode(['Mascot' => 'Mike']),
             json_encode(['Mascot' => 'Mike'])
         );
+    }
+
+
+    public function testGetJsonFunction()
+    {
+        $response = $this->http->request('GET', 'planets/1');
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json", $contentType);
+
+        $userAgent = json_decode($response->getBody())->{"planets/1"};
+        $this->assertRegexp('/Guzzle/', $userAgent);
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,7 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInitd36c096f7c2ff649ab91eb6a340b9608
 {
     public static $files = array (
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -18,6 +21,16 @@ class ComposerStaticInitd36c096f7c2ff649ab91eb6a340b9608
         'W' => 
         array (
             'Webmozart\\Assert\\' => 17,
+        ),
+        'P' => 
+        array (
+            'Psr\\Http\\Message\\' => 17,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
         ),
         'D' => 
         array (
@@ -36,6 +49,22 @@ class ComposerStaticInitd36c096f7c2ff649ab91eb6a340b9608
         'Webmozart\\Assert\\' => 
         array (
             0 => __DIR__ . '/..' . '/webmozart/assert/src',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
         'Doctrine\\Instantiator\\' => 
         array (
